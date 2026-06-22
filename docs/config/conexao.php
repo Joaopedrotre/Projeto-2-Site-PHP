590056edb7a85bf5d60bc314fbf85aee8ff425cd
João Trevisan <joao.trevisan@grupointegrado.br>
2026-06-22 17:23:44 -0300

    Preparando banco de dados em outra máquina

---
+++ docs/config/conexao.php
@@ -0,0 +1,25 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'autonobre_bd';
+$usuario = 'root';
+$senha = '';
+
+try {
+
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $usuario,
+        $senha
+    );
+
+    $pdo->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
+} catch (PDOException $e) {
+
+    die('Erro de conexão: ' . $e->getMessage());
+
+}
